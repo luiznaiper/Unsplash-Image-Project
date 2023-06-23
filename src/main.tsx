e00ff356576008f05e0955340ae429d3706e77e1
luiznaiper <luiz_naiper@hotmail.com>
2023-06-23 07:54:42 -0600

    :construction: Add QueryClientProvider, class 330

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AppProvider } from './context.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 )
